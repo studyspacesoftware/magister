5b3e1885ddc71d5556784e4d282cd4e7945365ca
Bart Wesselink <bartwesselink99@gmail.com>
2017-03-02 13:50:26 +0100

    Update MakeReplacements.php

--- src/Magister/Services/Foundation/Bootstrap/MakeReplacements.php
+++ src/Magister/Services/Foundation/Bootstrap/MakeReplacements.php
@@ -23,7 +23,7 @@ class MakeReplacements
         if ($app->auth->check()) {
             $id = $app->getId();
             if($id == null) {
-                $id = User::profile()->Id;
+                $id = User::profile()->Persoon['Id'];
                 $app->config->replace('url', 'id', $id);
                 if(Enrollment::current() != null) $app->config->replace('url', 'enrollment', Enrollment::current()->Id);
             } else {
